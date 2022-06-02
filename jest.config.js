b5763c1339713e9aa8d228028c7657faa49f3b10
nontangent <irregular0number@gmail.com>
2022-06-03 01:13:26 +0900

    chore: [nx migration] update-jest-config-to-use-util

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,3 @@
-module.exports = {
-  projects: [
-    '<rootDir>/apps/demo',
-    '<rootDir>/apps/api',
-    '<rootDir>/libs/common',
-    '<rootDir>/libs/firestore',
-    '<rootDir>/libs/domain',
-    '<rootDir>/libs/infrastructure',
-  ],
-};
+const { getJestProjects } = require('@nrwl/jest');
+
+module.exports = { projects: getJestProjects() };
